0dd7358459be015dbc88b79b7a9b9486a509214d
AndreasBrochs <andreas.brochs@outlook.com>
2021-04-08 16:17:00 +0200

    temp resolver in index

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,23 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 
 const schema = require("./schema");
-const resolvers = require("./resolver");
+// const resolvers = require("./resolver");
 const startDatabase = require("./database");
 
 const context = async () => {
   const db = await startDatabase();
   return { db };
+};
+
+const resolvers = {
+  events: async (_, context) => {
+    const { db } = await context();
+    return db.collection("events").find.toArray();
+  },
+  event: async ({ id }, context) => {
+    const { db } = await context();
+    return db.collection(events).findOne({ id });
+  },
 };
 
 const app = express();
@@ -16,7 +27,7 @@ app.use(
   graphqlHTTP({
     schema,
     rootValue: resolvers,
-    context
+    context,
   })
 );
 app.listen(4000);
